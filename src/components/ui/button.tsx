ac23d5d0cb10ada2993423daea4e5a9a297b5fbe
Frank Ezeh <ezehfrank87@gmail.com>
2023-12-02 23:12:54 +0100

    fix: update button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,36 +1,45 @@
 import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "[&>span]:hover:translate-y-0 [&>span:nth-child(2)]:hover:translate-y-[-100%] [&>span:nth-child(2)]:hover:p-2 relative overflow-hidden inline-flex items-center justify-center rounded-md text-lg font-bold z-20 text-white disabled:pointer-events-none disabled:opacity-50 hover:border-primary-blue transition-opacity duration-[250ms] ease-out",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] hover:to-[#3D8BFF]",
+        secondary: "border-[2px] border-primary-white",
+      },
+      size: {
+        sm: "hover:bg-primary-blue",
+        lg: "",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "lg",
+    },
+  }
+);
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: "primary" | "secondary";
-  size?: "sm" | "lg";
-}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
     return (
       <button
-        className={`[&>span]:hover:translate-y-0 [&>span:nth-child(2)]:hover:translate-y-[-101%] [&>span:nth-child(2)]:hover:p-2 
-        relative overflow-hidden inline-flex items-center justify-center rounded-md text-lg font-bold z-20
-        text-white disabled:pointer-events-none disabled:opacity-50  hover:border-primary-blue font-satoshi
-        ${
-          variant === "primary" &&
-          "bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] hover:to-[#3D8BFF]"
-        } ${
-          variant === "secondary" && "border-[2px] border-primary-white"
-        } ${className}`}
+        className={buttonVariants({ variant, size, className })}
         ref={ref}
       >
         <span
-          className={`absolute inset-0 bg-primary-blue transition-transform duration-300 ease-in 
-          translate-y-[101%] ${
-            variant === "primary" && size === "sm" && "p-2"
-          } ${variant === "primary" && size === "lg" && "p-3"}`}
+          className={`absolute inset-0 transition-transform duration-[250ms] ease-out translate-y-[100%] pt-2 lg:pt-3`}
           {...props}
         />
         <span
           {...props}
-          className="transition-transform duration-300 ease-in translate-y-0"
+          className="transition-transform duration-[250ms] ease-out translate-y-0"
         />
       </button>
     );
